1c38646fbcb67bfc7e6a34ad6f60d24ad685cb1d
kmh4321 <32885872+kmh4321@users.noreply.github.com>
2018-04-07 01:15:36 -0400

    Update Integration.m

--- Integration.m
+++ Integration.m
@@ -2,17 +2,72 @@ clear all
 close all
 clc
 %% Reading the Video
-obj = VideoReader('My Great Game - My Great Capture - 2018-04-04 17-57-51.mp4');
+obj = VideoReader('match.mp4');
 v = VideoWriter('v_1','MPEG-4');
 open(v);
-for frame_index = 1 : 5000 %fill in the appropriate number
+for frame_index = 1 : 600 %fill in the appropriate number
     disp(frame_index)
-    img = readFrame(obj);    
-
-    if(frame_index<350)
+    img = readFrame(obj);
+    if(frame_index<39)
         continue
     end
-    if(frame_index~=351 && mod(frame_index,20)~=0)
+    
+
+    if(frame_index==39)
+        imshow(img)
+        [x,y] = getpts;
+        points = [x,y];
+        close all
+        m = zeros(size(points,1)/2,1);
+        c = zeros(size(points,1)/2,1);
+        k = 1;
+        vp = zeros(2,1);
+        for j = 1:2:size(points,1)
+            m(k) = (points(j+1,2) - points(j,2))/ (points(j+1,1) - points(j,1));
+            c(k) = - points(j,1) * m(k) + points(j,2);
+            k = k+1;
+        end
+        count = 0;
+        for p = 1 : size(points,1)/2
+
+           for q = p+1 : size(points,1)/2
+               count = count + 1;
+               A = [-m(p),1;-m(q),1];
+               b = [c(p);c(q)];
+               vp = vp + inv(A)*b;
+           end
+        end
+        vp = int16(vp / count);
+        continue
+    end
+    
+    if(frame_index~=601 && mod(frame_index,20)~=0)
+        f = figure('visible','off');    
+        figure();
+        imshow(img)
+        hold on;
+        for i =1:size(S,1)
+            BB = S(i).BoundingBox;
+             if(A(i).Area < 4000 && A(i).Area > 650 && BB(3) < 110 && BB(4) < 110 && BB(3) > 20 && BB(4) > 20 && Team_Ids(i)~=0  && BB(1)>=temp_max)
+                rectangle('Position',[BB(1),BB(2),BB(3),BB(4)],...
+                'LineWidth',2,'EdgeColor','red')
+                if(Team_Ids(i)==1)
+                    text(BB(1)-2, BB(2)-2,'A');
+                end
+                if(Team_Ids(i)==2)
+                    text(BB(1)-2, BB(2)-2,'D');
+                end
+
+                if(Team_Ids(i)==3)
+                    text(BB(1)-2, BB(2)-2,'GK');
+                end
+
+             end
+        end
+        plot([left_most,vp(1)],[ly ,vp(2)],'y','LineWidth',1)
+        fig = getframe(gcf);
+        writeVideo(v,fig);
+        close(gcf);
         continue;
     end
     BW_img = rgb2gray(img);
@@ -21,10 +76,14 @@ for frame_index = 1 : 5000 %fill in the appropriate number
     start_angle = 89;
     end_angle = 89.99;
     theta_resolution = 0.01;
-    [hou,theta,rho] = hough(Edge_img, 'Theta', start_angle:theta_resolution:end_angle);
-    peaks = houghpeaks(hou,2,'threshold',ceil(0.3*max(hou(:))));
-    lines = houghlines(Edge_img,theta,rho,peaks,'FillGap',5,'MinLength',7);
     
+    [hou,theta,rho] = hough(Edge_img(1:floor(size(Edge_img,1)/2),:), 'Theta', start_angle:theta_resolution:end_angle);
+    peaks = houghpeaks(hou,2,'threshold',ceil(0.3*max(hou(:))));
+    lines = houghlines(Edge_img(1:floor(size(Edge_img,1)/2),:),theta,rho,peaks,'FillGap',5,'MinLength',7);
+%     [hou,theta,rho] = hough(Edge_img, 'Theta', start_angle:theta_resolution:end_angle);
+%     peaks = houghpeaks(hou,2,'threshold',ceil(0.3*max(hou(:))));
+%     lines = houghlines(Edge_img,theta,rho,peaks,'FillGap',5,'MinLength',7);
+%     
     min_row = lines(1).point1(2);
     xy_long = [lines(1).point1; lines(1).point2];
     for k = 1:length(lines) 
@@ -41,7 +100,7 @@ for frame_index = 1 : 5000 %fill in the appropriate number
     Edge_img(1:xy_long(:,2),:,:)=0;
    
 %%  Pre-Processing the image (Dilation - Opening - Erosion)
-    S_E_D = strel('disk',5);
+    S_E_D = strel('rectangle',[16,9]);
     Dilate_img = imdilate(Edge_img,S_E_D);
     S_E_E = strel('line',15,90);
     Open_img = bwareaopen(Dilate_img,1000);
@@ -67,35 +126,62 @@ for frame_index = 1 : 5000 %fill in the appropriate number
     Final = uint8(Erode_img).*img;
 
 %% Get the Attacker Seed
-    if(frame_index==1)
-        disp('Mark the attacker');
-        figure();imshow(img);
-        rect  = getrect;
-        Reqd_Img = Final(rect(2):rect(2)+rect(4)/2,rect(1):rect(1)+rect(3),:);
-        Attack_ID = mean(Reqd_Img(find(Reqd_Img>0)));
-    end
-    if(size(S,1)==0)
-        continue
-    end
-%%  M ark the Team-Players
+%     if(frame_index==1)
+%         disp('Mark the attacker');
+%         figure();imshow(img);
+%         rect  = getrect;
+%         Reqd_Img = Final(rect(2):rect(2)+rect(4)/2,rect(1):rect(1)+rect(3),:);
+%         Attack_ID = mean(Reqd_Img(find(Reqd_Img>0)));
+%     end
+%     if(size(S,1)==0)
+%         continue
+%     end
+%%  Mark the Team-Players
     Team_Ids = get_team_ids(Final,S,A);
 %% Mark the bounding boxes
     f = figure('visible','off');    
-%     figure();
+    figure();
     imshow(img)
     hold on;
+    temp_indices = find(Team_Ids == 3);
+    temp_BB = S(temp_indices);
+    temp_len = size(temp_BB);
+    temp_max = 0;
+    for t = 1:temp_len
+        if(temp_BB(t).BoundingBox(1)>temp_max)
+            temp_max = temp_BB(t).BoundingBox(1);
+        end
+    end
+    left_most = 9999;
     for i =1:size(S,1)
         BB = S(i).BoundingBox;
-         if(A(i).Area < 4000 && A(i).Area > 650 && BB(3) < 110 && BB(4) < 110 && BB(3) > 20 && BB(4) > 20 )
+         if(A(i).Area < 4000 && A(i).Area > 650 && BB(3) < 110 && BB(4) < 110 && BB(3) > 20 && BB(4) > 20 && Team_Ids(i)~=0  && BB(1)>=temp_max)
             rectangle('Position',[BB(1),BB(2),BB(3),BB(4)],...
             'LineWidth',2,'EdgeColor','red')
             if(Team_Ids(i)==1)
                 text(BB(1)-2, BB(2)-2,'A');
-            else
+            end
+            if(Team_Ids(i)==2)
                 text(BB(1)-2, BB(2)-2,'D');
             end
+            
+            if(Team_Ids(i)==3)
+                text(BB(1)-2, BB(2)-2,'GK');
+            end
+            x1 = floor(BB(1) + BB(3)/2);
+            y1 = floor(BB(2) + BB(4));
+            ly = size(img,1);
+            slope = (vp(2) - y1)/(vp(1) - x1);
+            y_int = - x1 * slope + y1;
+            lx = (ly - y_int)/slope;
+            if(lx<left_most && Team_Ids(i) == 1)
+             left_most = lx;
+            end
+            
          end
+         
     end
+    plot([left_most,vp(1)],[ly ,vp(2)],'y','LineWidth',1)
     fig = getframe(gcf);
     writeVideo(v,fig);
     close(gcf);
